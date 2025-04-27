e7fa3803ff03ad3c364a5b47d537fc3fc3571336
Víctor Mardones <vmardones.dev@gmail.com>
2025-04-26 21:08:58 -0400

    fix: Corrige blueprint no registrado

--- chatbot-pds-api/chatbot_project/chatbot_api.py
+++ chatbot-pds-api/chatbot_project/chatbot_api.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_cors import CORS
 from werkzeug.middleware.proxy_fix import ProxyFix
+from modules.chatbot_bp import chatbot_bp
 
 app = Flask(__name__)
 CORS(app)
 app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+app.register_blueprint(chatbot_bp)
 
 if __name__ == '__main__':
     app.run()
